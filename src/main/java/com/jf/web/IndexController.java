8bd54d26f1ee7743b9c71b03fb07c10d92e59a56
jspp@qq.com <123456>
2019-04-10 13:12:41 +0800

    init

---
+++ src/main/java/com/jf/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.jf.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 国际化 测试
+ * ouyangjie
+ * 2019/4/10
+ * 13:10
+ */
+@Controller
+public class IndexController {
+
+
+
+}
